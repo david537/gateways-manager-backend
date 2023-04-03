f2b850e2e97b7256d95b314ca6533fecfb4e6e81
David <davidalvarezsantes@gmail.com>
2023-04-03 12:21:53 -0400

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gateways')
+    .setDescription('This sample project is managing gateways - master devices that control multiple peripheral devices. ')
+    .setVersion('1.0')
+    .addTag('gateways')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
