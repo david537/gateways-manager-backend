6a0bc969e54db44f7eb87c3974b2713b42d2ec0b
David <davidalvarezsantes@gmail.com>
2023-04-04 10:16:24 -0400

    update gateway entity

--- src/gateways/entities/gateway.entity.ts
+++ src/gateways/entities/gateway.entity.ts
@@ -1,1 +1,16 @@
-export class Gateway {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Gateway } from '@prisma/client';
+
+export class GatewayEntity implements Gateway {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  serial: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  ip: string;
+}
