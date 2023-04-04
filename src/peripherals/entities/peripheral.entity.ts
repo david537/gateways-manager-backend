433b694220fe32ccaadac1e944cb043f5e4a877e
David <davidalvarezsantes@gmail.com>
2023-04-04 11:25:53 -0400

    add resources to peripheral entity

--- src/peripherals/entities/peripheral.entity.ts
+++ src/peripherals/entities/peripheral.entity.ts
@@ -1,1 +1,22 @@
-export class Peripheral {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Peripheral } from '@prisma/client';
+
+export class PeripheralEntity implements Peripheral {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  uid: number;
+
+  @ApiProperty()
+  vendor: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  status: string;
+
+  @ApiProperty()
+  gatewayId: string;
+}
