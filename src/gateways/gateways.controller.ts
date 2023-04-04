2e1dad4c357503a342494d62b47e93ff75ed1bf4
David <davidalvarezsantes@gmail.com>
2023-04-04 10:17:26 -0400

    add swagger to gateway controller

--- src/gateways/gateways.controller.ts
+++ src/gateways/gateways.controller.ts
@@ -10,7 +10,8 @@ import {
 import { GatewaysService } from './gateways.service';
 import { CreateGatewayDto } from './dto/create-gateway.dto';
 import { UpdateGatewayDto } from './dto/update-gateway.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { GatewayEntity } from './entities/gateway.entity';
 
 @ApiTags('gateways')
 @Controller('gateways')
@@ -18,26 +19,31 @@ export class GatewaysController {
   constructor(private readonly gatewaysService: GatewaysService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: GatewayEntity })
   create(@Body() createGatewayDto: CreateGatewayDto) {
     return this.gatewaysService.create(createGatewayDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: GatewayEntity, isArray: true })
   findAll() {
     return this.gatewaysService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: GatewayEntity })
   findOne(@Param('id') id: string) {
     return this.gatewaysService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: GatewayEntity })
   update(@Param('id') id: string, @Body() updateGatewayDto: UpdateGatewayDto) {
     return this.gatewaysService.update(id, updateGatewayDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: GatewayEntity })
   remove(@Param('id') id: string) {
     return this.gatewaysService.remove(id);
   }
