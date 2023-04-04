0f79bb8b805908fc392ed62b601bfd96731578bb
David <davidalvarezsantes@gmail.com>
2023-04-04 11:26:20 -0400

    add validator dto peripheral

--- src/peripherals/dto/create-peripheral.dto.ts
+++ src/peripherals/dto/create-peripheral.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePeripheralDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsInt, IsDate } from 'class-validator';
+
+export class CreatePeripheralDto {
+  @ApiProperty()
+  @IsInt()
+  readonly serial: number;
+
+  @ApiProperty()
+  @IsString()
+  readonly vendor: string;
+
+  @ApiProperty()
+  @IsString()
+  readonly status: string;
+
+  @ApiProperty()
+  @IsDate()
+  readonly createAt: Date;
+
+  @ApiProperty()
+  @IsString()
+  readonly gatewayId: string;
+}
