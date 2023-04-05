762e63deac86c96f1063de0a7d0a30f660eda585
David <davidalvarezsantes@gmail.com>
2023-04-05 13:40:24 -0400

    to improve the catch and treatment of errors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { PrismaExceptionFilter } from './prisma-exception/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   const config = new DocumentBuilder()
     .setTitle('Gateways')
     .setDescription(
-      'This sample project is managing gateways - master devices that control multiple peripheral devices. ',
+      'This sample project is managing gateways - master devices that control multiple peripheral devices. '
     )
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter));
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
