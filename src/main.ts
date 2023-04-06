30ce8fa98eafe8cf3e5df5ba47bf2c65f57da3a5
David <davidalvarezsantes@gmail.com>
2023-04-06 13:40:40 -0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './prisma-exception/prisma-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
